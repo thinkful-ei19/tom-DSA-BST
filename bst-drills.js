6e7b2f778a8ded34d05882825662e79bccd13277
thaborak <thaborak@gmail.com>
2018-05-22 12:13:52 -0400

    create a BST class

--- bst-drills.js
+++ bst-drills.js
@@ -143,3 +143,23 @@ class BinarySearchTree {
     return this.left._findMin();
   }
 }
+
+
+function main() {
+  const bst = new BinarySearchTree();
+  bst.insert(3,3);
+  bst.insert(1,1);
+  bst.insert(4,4);
+  bst.insert(6,6);
+  bst.insert(9.9);
+  bst.insert(2,2);
+  bst.insert(5,5);
+  bst.insert(7,7);
+  //console.log(JSON.stringify(bst));
+  console.log(bst);
+}
+
+main();
+// Height of a BST
+// Write an algorithm to find the height of a binary search tree. What is the run time of your algorithm?
+
